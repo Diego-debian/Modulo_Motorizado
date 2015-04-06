1699889756e1a308eccf39f1301bfcf427171102
Diego-debian <diegoestudianteud1@gmail.com>
2015-04-06 01:13:49 -0500

    montajes

---
+++ 2. Interfaz/2.2 Grafica/prograsecu/Ayuda/montaje1.py
@@ -0,0 +1,24 @@
+from visual import *
+x= -8
+frente1 = box(size = (1, 5, 0.6), pos = (0+x, 0, 0), color = color.red)
+frente2 = box(size = (3, 3, 0.6), pos =(2+x, 0 ,0), color = color.red)
+frente3 = box(size = (7.5, 5, 0.6), pos =(7+x, 0 , 0), color = color.red)
+frente4 = box(size = (1.5, 3, 0.6), pos =(8+x, 4 , 0), color = color.red)
+frente5 = box(size = (1.5, 3, 0.6), pos =(8+x, -4 , 0), color = color.red) 
+frente6 = box(size = (5, 11, 0.6), pos =(13.1+x, 0 , 0), color = color.red)
+frente7 = box(size = (1, 3.5, 0.6), pos =(16+x, 3.8 , 0), color = color.red)
+frente8 = box(size = (1, 2.5, 0.6), pos =(16+x, -4.3 , 0), color = color.red)  
+frente9 = box(size = (2, 2, 0.6), pos =(9.6+x, 3.3 , 0), color = color.red)  
+frente10 = box(size = (2, 2, 0.6), pos =(9.6+x, -3.3 , 0), color = color.red)  
+
+#caja1 = box(size = (0.2, 30, 0.6), pos =(-15, 0 ,0), color = color.white)
+#caja2 = box(size = (0.2, 30, 0.6), pos =(15, 0 ,0), color = color.white)
+#caja3 = box(size = (30, 0.2, 0.6), pos =(0, 15 ,0), color = color.white)
+#caja4 = box(size = (30, 0.2, 0.6), pos =(0, -15 ,0), color = color.white)
+
+cilindro1 = cylinder(pos=(-6,0,-2), axis=(0,0,3), radius=(0.7), color = color.white)
+cilindro2 = cylinder(pos=(-5.92, -3.5, -1), axis=(0,7,0), radius=(0.2), color = color.white)
+cilindro3 = cylinder(pos=(-5.92, -5, -1), axis=(0,1.4,0), radius=(1.4), color = color.white)
+cilindro4 = cylinder(pos=(-5.92, 3.5, -1), axis=(0,1.4,0), radius=(1.4), color = color.white)
+
+#label0a = label(pos = (-7, -7 ,0 ), text= 'DIEGO ALBERTO PARRA GARZON',  font='sans', box = True, height = 12)
